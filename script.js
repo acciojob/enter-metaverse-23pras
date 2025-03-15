9a4475e4fe7685405553ee7bd301a82b2d8bd593
Prashant Pal <palprashant465@gmail.com>
2025-03-15 12:20:06 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,11 @@
 //your JS code here. If required.
+
+let ptag = document.getElementById('status');
+let button = document.getElementById("enterBtn");
+
+button.addEventListener("click",()=>{
+	  let h1Tag = document.createElement("h1");
+      h1Tag.textContent = "Entered Metaverse";
+
+      ptag.replaceWith(h1Tag);
+})
